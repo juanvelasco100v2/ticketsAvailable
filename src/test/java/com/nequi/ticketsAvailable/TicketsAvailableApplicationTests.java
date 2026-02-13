c53c8c682e20771283c7f5ece625d55373d77e0b
juanmanuelvelasco <juanvelasco100@gmail.com>
2026-02-12 21:16:19 -0500

    Initial commit

---
+++ src/test/java/com/nequi/ticketsAvailable/TicketsAvailableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.ticketsAvailable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsAvailableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
